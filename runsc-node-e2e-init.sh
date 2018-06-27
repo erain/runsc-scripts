5b89c5d4bfdedc6ef4533c0168a8548270b5ab7b
Lantao Liu <lantaol@google.com>
2018-06-27 00:29:05 -0700

    Make private variables private.
    Signed-off-by: Lantao Liu <lantaol@google.com>

--- runsc-node-e2e-init.sh
+++ runsc-node-e2e-init.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 # CONTAINERD_HOME is the directory for containerd.
-CONTAINERD_HOME=${CONTAINERD_HOME:-"/home/containerd"}
+CONTAINERD_HOME="/home/containerd"
 
 # KUBE_ENV_METADATA is the metadata key for kubernetes envs.
 KUBE_ENV_METADATA="kube-env"
@@ -19,11 +19,11 @@ if [ -f "${CONTAINERD_HOME}/${CONTAINERD_ENV_METADATA}" ]; then
   source "${CONTAINERD_HOME}/${CONTAINERD_ENV_METADATA}"
 fi
 
-# CONTAINERD_CONFIG_PATH is the path of containerd config file.
-CONTAINERD_CONFIG_PATH=${CONTAINERD_CONFIG_PATH:-"/etc/containerd/config.toml"}
+# containerd_config_path is the path of containerd config file.
+containerd_config_path=${CONTAINERD_CONFIG_PATH:-"/etc/containerd/config.toml"}
 
-# RUNSC_DEPLOY_PATH is the path to deploy runsc binary.
-RUNSC_DEPLOY_PATH=${RUNSC_DEPLOY_PATH:-"cri-containerd-staging/runsc"}
+# runsc_deploy_path is the path to deploy runsc binary.
+runsc_deploy_path=${RUNSC_DEPLOY_PATH:-"cri-containerd-staging/runsc"}
 
 # Initialize containerized mounter.
 mount /tmp /tmp -o remount,exec,suid
@@ -42,11 +42,11 @@ rm /tmp/mounter.tar
 
 # Download runsc.
 runsc_bin_name=$(curl -f --ipv4 --retry 6 --retry-delay 3 --silent --show-error \
-  "https://storage.googleapis.com/${RUNSC_DEPLOY_PATH}/latest")
+  "https://storage.googleapis.com/${runsc_deploy_path}/latest")
 runsc_bin_path="${CONTAINERD_HOME}/usr/local/sbin/runsc"
 curl -f --ipv4 -Lo "${runsc_bin_path}" --connect-timeout 20 --max-time 300 \
-  --retry 6 --retry-delay 10 "https://storage.googleapis.com/${RUNSC_DEPLOY_PATH}/${runsc_bin_name}"
+  --retry 6 --retry-delay 10 "https://storage.googleapis.com/${runsc_deploy_path}/${runsc_bin_name}"
 chmod 755 "${runsc_bin_path}"
 
 # Update containerd config to use runsc.
-sed -i 's/runc/runsc/g' "${CONTAINERD_CONFIG_PATH}"
+sed -i 's/runc/runsc/g' "${containerd_config_path}"
